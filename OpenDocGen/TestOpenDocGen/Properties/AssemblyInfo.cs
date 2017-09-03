3ed5e2f3aa01cff379a44a7e78655bdc4087045d
Christian von Seydlitz <cvs@von-seydlitz.de>
2017-09-03 11:52:30 +0200

    Basic with test

---
+++ OpenDocGen/TestOpenDocGen/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestOpenDocGen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestOpenDocGen")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("289c50bc-96db-4d27-88e4-47d2a98ae5e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OpenDocGen/TestOpenDocGen/TestConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using OpenDocGen;
+
+namespace TestOpenDocGen
+{
+    [TestClass]
+    public class TestConverter
+    {
+
+        private TestContext testContextInstance;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+
+        [TestMethod]
+        public void TestConvert2Html()
+        {
+            /**
+             * This example loads each document into a byte array, then into a memory stream, so that the document can be opened for writing without
+             * modifying the source document.
+             */
+            var docPath = "../../data";
+            var outPath = "../../out";
+            Assert.IsTrue(Directory.Exists(docPath));
+
+            foreach (var file in Directory.GetFiles(docPath, "*.docx"))
+            {
+                TestContext.WriteLine($"Converting Document {file} to Path: {outPath}");
+                var converter = new HtmlConverter();
+                converter.ConvertToHtml(file, outPath);
+            }
+        }
+    }
+}
